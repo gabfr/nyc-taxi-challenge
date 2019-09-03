ed694aaadc6014c9a132938c6876ce49d7c34112
Gabriel <eu@gabrielf.com>
2019-09-03 03:05:19 -0300

    feature: creating the plugins folder, for guarding common operators adding the basic operators always used in the projects

---
+++ plugins/operators/__init__.py
@@ -0,0 +1,14 @@
+from operators.stage_json_to_redshift import StageJsonToRedshiftOperator
+from operators.stage_csv_to_redshift import StageCsvToRedshiftOperator
+from operators.load_fact import LoadFactOperator
+from operators.load_dimension import LoadDimensionOperator
+from operators.data_quality import DataQualityOperator
+
+
+__all__ = [
+    'StageJsonToRedshiftOperator',
+    'StageCsvToRedshiftOperator',
+    'LoadFactOperator',
+    'LoadDimensionOperator',
+    'DataQualityOperator',
+]
---
+++ plugins/operators/data_quality.py
@@ -0,0 +1,33 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class DataQualityOperator(BaseOperator):
+
+    ui_color = '#89DA59'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="",
+                 tables=[],
+                 where_parameters=None,
+                 *args, **kwargs):
+
+        super(DataQualityOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.tables = tables
+        self.where_parameters = where_parameters
+
+    def execute(self, context):
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        where_params = ''
+        if self.where_parameters is not None:
+            where_params = f"WHERE {self.where_parameters}"
+        for table in self.tables:
+            records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table} {where_params}")
+            if len(records) < 1 or len(records[0]) < 1:
+                raise ValueError(f"Data quality check failed. {table} returned no results")
+            num_records = records[0][0]
+            if num_records < 1:
+                raise ValueError(f"Data quality check failed. {table} contained 0 rows")
+            self.log.info(f"Data quality on table {table} check passed with {records[0][0]} records")
