2ea8d85b11133b9ce6f21a2e18548b6349b82243
Gabriel <eu@gabrielf.com>
2019-09-03 03:09:09 -0300

    feature: the first DAG. The beggining of everything. Creating the default business intelligence tables

---
+++ dags/recreate_bi_tables.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators import PostgresOperator
+from helpers import SqlQueries
+
+
+default_args = {
+    'owner': 'gabriel',
+    'start_date': datetime(2009, 12, 31, 23, 59, 59),
+    'end_date': datetime(2012, 12, 31, 23, 59, 59),
+    'depends_on_past': False,
+    'retries': 1,
+    'retry_delay': timedelta(seconds=300),
+    'catchup': True
+}
+
+dag = DAG('recreate_bi_tables',
+          default_args=default_args,
+          description='Recreate the business intelligence tables',
+          schedule_interval=None,
+          max_active_runs=1
+        )
+
+recreate_bi_tables = PostgresOperator(
+    task_id="recreate_bi_tables_task",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=SqlQueries.recreate_bi_tables
+)
+
+recreate_bi_tables
